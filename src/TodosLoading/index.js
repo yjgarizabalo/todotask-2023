4b0104a8e749e550bb96e9a2fcc9dfeaf0e41769
yairg <yjgarizabalo@gmail.com>
2023-03-09 17:45:50 -0500

    [todo] loading

--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import '../assets/css/TodosLoading.css'
 
 function TodosLoading() {
-  return <p>Estamos Cargando, no desesperes...</p>;
+  return (
+    <div className='loadingTodo-container'>
+      <span className='loadingTodo-completeIcon'></span>
+      <p className='loadinTodo-text'>Carando TODOS...</p>
+      <span className='ladingTodo-deleteIcon'></span>
+    </div>
+  )
 }
 
 export { TodosLoading };
